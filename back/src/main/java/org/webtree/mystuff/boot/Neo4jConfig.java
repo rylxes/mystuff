f2b0e182d53032ad897c35fed2a939364aa52315
Maxim Levitskiy <max.dstu@gmail.com>
2018-05-07 17:44:22 +0200

    #20_Fix_backend-neo4j_connection_in_docker_without_update

--- back/src/main/java/org/webtree/mystuff/boot/Neo4jConfig.java
+++ back/src/main/java/org/webtree/mystuff/boot/Neo4jConfig.java
@@ -1,19 +1,39 @@
 package org.webtree.mystuff.boot;
 
+import com.google.common.base.Strings;
+import org.neo4j.ogm.config.Configuration;
+import org.neo4j.ogm.config.DriverConfiguration;
 import org.neo4j.ogm.session.SessionFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.data.neo4j.config.Neo4jConfiguration;
 import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@Configuration
+@org.springframework.context.annotation.Configuration
 @EnableTransactionManagement
 @ComponentScan("org.webtree.mystuff")
 @EnableNeo4jRepositories("org.webtree.mystuff.repository")
 public class Neo4jConfig extends Neo4jConfiguration {
+    @Value("${spring.data.neo4j.driver}")
+    private String driver;
+    @Value("${spring.data.neo4j.uri:#{null}}")
+    private String uri;
+
     @Override
     public SessionFactory getSessionFactory() {
-        return new SessionFactory("org.webtree.mystuff.domain");
+        return new SessionFactory(configuration(), "org.webtree.mystuff.domain");
+    }
+
+    @Bean
+    public org.neo4j.ogm.config.Configuration configuration() {
+        Configuration config = new Configuration();
+        DriverConfiguration driverConfiguration = config.driverConfiguration();
+        driverConfiguration.setDriverClassName(driver);
+        if (!Strings.isNullOrEmpty(uri)) {
+            driverConfiguration.setURI(uri);
+        }
+        return config;
     }
 }
