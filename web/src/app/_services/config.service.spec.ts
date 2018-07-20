4594262018b9ac2620c8ae549d2fc7db80708540
levim2 <maxim.levitskiy@pega.com>
2018-07-20 15:07:41 +0000

    #20 front+back+neo4j containers config

--- web/src/app/_services/config.service.spec.ts
+++ web/src/app/_services/config.service.spec.ts
@@ -1,23 +1,30 @@
 import {TestBed} from "@angular/core/testing";
 
 import {ConfigService} from "./config.service";
-
-
+import { CookieService } from 'ngx-cookie-service';
 
 describe('ConfigService', () => {
 
   let configService: ConfigService;
+  let cookieService: CookieService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [ConfigService],
+      providers: [ConfigService, CookieService],
     });
     configService = TestBed.get(ConfigService);
+    cookieService = TestBed.get(CookieService);
   });
 
   it('#getBackUrl should return expected value', () => {
     const url = "http://localhost:9000";
     expect(configService.getBackUrl()).toEqual(url);
+  });
 
+  it('#getBackUrl should return value from cookie', () => {
+    let urlFromCookie = "someUrlFromCookie";
+    spyOn(cookieService, 'get').and.returnValue(urlFromCookie);
+    spyOn(cookieService, 'check').and.returnValue(true);
+    expect(configService.getBackUrl()).toEqual(urlFromCookie);
   });
 });
